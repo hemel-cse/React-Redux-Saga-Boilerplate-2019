021c2f4da448a611c7eaace0fcb3146fb34f2074
Md Hashibul Amin <hasibul.amin.hemel@gmail.com>
2018-10-05 03:48:35 +0600

    update logger

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import createSagaMiddleware from 'redux-saga';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import { createLogger } from 'redux-logger';
 import rootReducer from './reducers';
 import sagas from './sagas';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker'; 
 
+
+const middleware = [];
 const sagaMiddleware = createSagaMiddleware();
+
+middleware.push(sagaMiddleware);
+middleware.push(createLogger()); // use checker when in developments or remove from production
+
+const enhancers = [applyMiddleware(...middleware)];
+
 const store = createStore(
   rootReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
    compose(
-	applyMiddleware(sagaMiddleware),
+	...enhancers
     )
 );
 
